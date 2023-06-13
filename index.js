59f57ba2e82cef7486813013f0293217cd3f0581
Tabitha <flor.palida@yahoo.com>
2023-06-13 12:44:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+//const companyName = Scuber;
+//companyName = Scuber;
+let mostProfitableNeighborhood = `Chelsea`;
+//  is declared as equal to Chelsea:
+let companyCeo = `Susan Smith`;
